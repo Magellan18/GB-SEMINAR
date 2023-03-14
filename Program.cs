384f45c13d07f4d8378d879a7914f720a08568bc
Magellan18 <123990408+Magellan18@users.noreply.github.com>
2023-03-14 15:30:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,75 +1,173 @@
 // using System;
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         int[] array = new int[4];
-//         Random rnd = new Random();
+// class Program {
+//     static void Main() {
+//         int[,] arr = {
+//             {1, 4, 7, 2},
+//             {5, 9, 2, 3},
+//             {8, 4, 2, 4}
+//         };
 
-//         for (int i = 0; i < array.Length; i++)
-//         {
-//             array[i] = rnd.Next(100, 1000);
-//         }
-//         int count = 0;
-//         foreach (int number in array)
-//         {
-//             if (number % 2 == 0)
-//             {
-//                 count++;
+//         int rows = arr.GetLength(0);
+//         int cols = arr.GetLength(1);
+
+//         for (int i = 0; i < rows; i++) {
+//             for (int j = 0; j < cols - 1; j++) {
+//                 for (int k = j + 1; k < cols; k++) {
+//                     if (arr[i,j] < arr[i,k]) {
+//                         int temp = arr[i,j];
+//                         arr[i,j] = arr[i,k];
+//                         arr[i,k] = temp;
+//                     }
+//                 }
 //             }
 //         }
 
-//         Console.WriteLine($"Количество четных чисел в массиве: {count}");
+//         for (int i = 0; i < rows; i++) {
+//             for (int j = 0; j < cols; j++) {
+//                 Console.Write(arr[i,j] + " ");
+//             }
+//             Console.WriteLine();
+//         }
 //     }
 // }
 
 // using System;
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         int[] array = new int[4];
-//         Random rnd = new Random();
-//         for (int i = 0; i < array.Length; i++)
-//         {
-//             array[i] = rnd.Next(-100, 100);
-//         }
-//         int sum = 0;
-//         for (int i = 1; i < array.Length; i += 2)
-//         {
-//             sum += array[i];
+// class Program {
+//     static void Main() {
+//         int[,] arr = {
+//             {1, 4, 7, 2},
+//             {5, 9, 2, 3},
+//             {8, 4, 2, 4},
+//             {5, 2, 6, 7}
+//         };
+
+//         int rows = arr.GetLength(0);
+//         int cols = arr.GetLength(1);
+
+//         int minSum = int.MaxValue;
+//         int minIndex = -1;
+
+//         for (int i = 0; i < rows; i++) {
+//             int sum = 0;
+//             for (int j = 0; j < cols; j++) {
+//                 sum += arr[i,j];
+//             }
+//             if (sum < minSum) {
+//                 minSum = sum;
+//                 minIndex = i;
+//             }
 //         }
 
-//         Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях: {sum}");
+//         Console.WriteLine("Наименьшая сумма элементов в строке {0}", minIndex + 1);
 //     }
 // }
-using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        double[] array = { 3.0, 7.0, 22.0, 2.0, 78.0 };
-        double max = array[0];
-        double min = array[0];
+// using System;
 
-        for (int i = 1; i < array.Length; i++)
-        {
-            if (array[i] > max)
-            {
-                max = array[i];
-            }
+// class Program {
+//     static void Main() {
+//         int[,] matrix1 = {
+//             {4, 5},
+//             {3, 1}
+//         };
+//         int[,] matrix2 = {
+//             {4, 5},
+//             {3, 1}
+//         };
+//         int rows1 = matrix1.GetLength(0);
+//         int cols1 = matrix1.GetLength(1);
+//         int rows2 = matrix2.GetLength(0);
+//         int cols2 = matrix2.GetLength(1);
+//         if (cols1 != rows2) {
+//             Console.WriteLine("Невозможно выполнить умножение матриц");
+//             return;
+//         }
+//         int[,] result = new int[rows1, cols2];
+//         for (int i = 0; i < rows1; i++) {
+//             for (int j = 0; j < cols2; j++) {
+//                 int sum = 0;
+//                 for (int k = 0; k < cols1; k++) {
+//                     sum += matrix1[i,k] * matrix2[k,j];
+//                 }
+//                 result[i,j] = sum;
+//             }
+//         }
+//         Console.WriteLine("Результирующая матрица:");
+//         for (int i = 0; i < rows1; i++) {
+//             for (int j = 0; j < cols2; j++) {
+//                 Console.Write("{0} ", result[i,j]);
+//             }
+//             Console.WriteLine();
+//         }
+//     }
+// }
 
-            if (array[i] < min)
-            {
-                min = array[i];
-            }
-        }
+// using System;
 
-        double diff = max - min;
+// class Program {
+//     static void Main() {
+//         int[,,] array = new int[2, 2, 2];
+//         int count = 10;
+//         for (int i = 0; i < 2; i++) {
+//             for (int j = 0; j < 2; j++) {
+//                 for (int k = 0; k < 2; k++) {
+//                     array[i,j,k] = count;
+//                     count++;
+//                 }
+//             }
+//         }
+//         Console.WriteLine("Массив:");
+//         for (int i = 0; i < 2; i++) {
+//             for (int j = 0; j < 2; j++) {
+//                 for (int k = 0; k < 2; k++) {
+//                     Console.Write("{0}({1},{2},{3}) ", array[i,j,k], i, j, k);
+//                 }
+//                 Console.WriteLine();
+//             }
+//             Console.WriteLine();
+//         }
+//     }
+// }
+// using System;
 
-        Console.WriteLine($"Разница между максимальным и минимальным элементами: {diff}");
-    }
-}
+// class Program {
+//     static void Main() {
+//         int[,] array = new int[4, 4];
+//         int n = 4;
+//         int count = 1;
+//         int i, j;
+//         int k = 0;
+//         int l = 0;
+//         while (k < n && l < n) {
+//             for (i = l; i < n; i++) {
+//                 array[k,i] = count++;
+//             }
+//             k++;
+//             for (i = k; i < n; i++) {
+//                 array[i,n-1] = count++;
+//             }
+//             n--;
+//             if (k < n) {
+//                 for (i = n-1; i >= l; i--) {
+//                     array[n-1,i] = count++;
+//                 }
+//                 n--;
+//             }
+//             if (l < n) {
+//                 for (i = n-1; i >= k; i--) {
+//                     array[i,l] = count++;
+//                 }
+//                 l++;
+//             }
+//         }
+//         Console.WriteLine("Массив:");
+//         for (i = 0; i < 4; i++) {
+//             for (j = 0; j < 4; j++) {
+//                 Console.Write("{0:D2} ", array[i,j]);
+//             }
+//             Console.WriteLine();
+//         }
+//     }
+// }
